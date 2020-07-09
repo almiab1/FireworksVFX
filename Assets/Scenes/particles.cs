1a28a2643f97d80d9d295616881111d3daf21817
almiab1 <almiab1@epsg.upv.es>
2020-07-09 20:08:10 +0200

    fireworks

---
+++ Assets/Scenes/particles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class particles : MonoBehaviour
+{
+    public VisualEffect myEffect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myEffect.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
